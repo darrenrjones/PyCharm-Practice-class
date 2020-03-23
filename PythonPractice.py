986c5e1015bf5402851f4d54959b8325e297ac43
darrenrjones <ninenumbproductions@gmail.com>
2020-03-23 15:49:31 -0700

    created less than equal than

---
+++ PythonPractice.py
@@ -0,0 +1,53 @@
+def main():
+    print("Finally we are pythoning")
+
+if __name__ == "__main__":
+    main()
+
+name = "Billy Bob"
+
+# print("Hello {}!".format(name))
+
+# 2. Create a variable called x and set it to 10. Print x to the
+# console.
+
+def lessThanGreaterThan(number):
+
+    if number > 10:
+        print("{} is bigger than 10".format(number))
+    if number < 10:
+        print("{} is less than 10".format(number))
+    if number == 10:
+        print("{} is equal to 10".format(number))
+
+lessThanGreaterThan(10)
+
+
+
+
+
+# 3. Write a program that takes a number and tells the user if the
+# number is greater than 10, less than 10, or equal to 10. Don't
+# forget to convert the string into an integer.
+
+# 4. Write a program that takes a number as a user input and tells
+# the user if it is even or odd.
+
+# 5. Create a list and write a program that prints each individual
+# element to the console.
+
+# 6. Write a program that uses a loop fill an empty list with 10
+# numbers and prints the list to the console. The numbers can
+# be random or simply put in the numbers 0-9 or 1-10 in
+# ascending order.
+
+# 7. Write a program that takes a number as user input and tells
+# you how many numbers in a list are less than the number that
+# the user gave.
+
+# 8. Write a program that keeps asking the user to enter a word.
+# For each word that the user enters, the program prints out
+# each letter in the word on a new line. When the user enters
+# “s
+
+
